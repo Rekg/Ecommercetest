2f0c3d6e660c0796edb6f648fa14d239b92b6d2e
Rekg <106755026+Rekg@users.noreply.github.com>
2026-01-02 11:51:27 -0800

    Add AuthService unit tests and update app.spec.ts

---
+++ src/app/core/services/auth.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule, 
+      ],
+      providers: [
+        AuthService,
+        provideMockStore({ initialState: {} }), 
+      ],
+    });
+
+    service = TestBed.inject(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return false when not logged in', () => {
+    expect(service.isLoggedIn()).toBeFalse();
+  });
+});
