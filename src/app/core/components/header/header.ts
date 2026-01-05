c03f6249865f603ab549ac7f19bde3625f298f22
Rekg <106755026+Rekg@users.noreply.github.com>
2026-01-05 11:53:28 -0800

    nav fix modal and mobile

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Store } from '@ngrx/store'; 
-import { selectCartItemsCount } from '../../../features/shopping-cart/state/cart.selectors'; 
-import { Observable, map } from 'rxjs'; 
-import { WishlistStateService } from '../../../features/wishlist/services/wishlist-state.service'; 
+import { Store } from '@ngrx/store';
+import { selectCartItemsCount } from '../../../features/shopping-cart/state/cart.selectors';
+import { Observable, map } from 'rxjs';
+import { WishlistStateService } from '../../../features/wishlist/services/wishlist-state.service';
 import { AuthService } from '../../services/auth.service';
 import { CartActions } from '../../../features/shopping-cart/state/cart.actions';
 
@@ -12,20 +12,27 @@ import { CartActions } from '../../../features/shopping-cart/state/cart.actions'
   standalone: true,
   selector: 'app-header',
   imports: [CommonModule, RouterModule],
-  templateUrl:'./header.html' ,
+  templateUrl: './header.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   private store = inject(Store);
   private router = inject(Router);
   public authService = inject(AuthService);
-  private wishlistService = inject(WishlistStateService); 
+  private wishlistService = inject(WishlistStateService);
 
-  cartTotalQuantity$: Observable<number> = this.store.select(selectCartItemsCount).pipe(
-    map(count => this.authService.isLoggedIn() ? count : 0)
-  );
-  
+  isMobileMenuOpen = signal(false);
+  showLogoutModal = signal(false);
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen.update((v) => !v);
+  }
+
+  cartTotalQuantity$: Observable<number> = this.store
+    .select(selectCartItemsCount)
+    .pipe(map((count) => (this.authService.isLoggedIn() ? count : 0)));
+
   wishlistCount$: Observable<number> = this.wishlistService.wishlist$.pipe(
-    map(items => items.length)
+    map((items) => items.length)
   );
 
   ngOnInit() {
@@ -42,8 +49,17 @@ export class HeaderComponent {
     return this.authService.getRole() === 'ADMIN';
   }
 
-  onLogout() {
-    this.authService.logout();
-    this.router.navigate(['/auth/login']);
+  
+  confirmLogout() {
+    this.showLogoutModal.set(true);
+    if (this.isMobileMenuOpen()) this.isMobileMenuOpen.set(false);
+  }
+
+  
+  handleLogoutResponse(confirmed: boolean) {
+    this.showLogoutModal.set(false);
+    if (confirmed) {
+      this.authService.logout();
+    }
   }
 }
